a62185a521ca1b949456041ec51c6d65f99f3d45
Natannms <60887035+Natannms@users.noreply.github.com>
2022-05-06 00:38:05 -0300

    Add Sql for create user table in postgres, and insert users in table

---
+++ SQL/User.sql
@@ -0,0 +1,16 @@
+
+
+create table user{
+    id serial primary key,
+    name varchar not null,
+    email varchar not null,
+    password varchar not null,
+}
+
+INSERT INTO user (name, email, password) VALUES 
+('Ronaldo', 'teste@gmail.com' , '123456'),
+('Everton', 'teste@gmail.com', '123456'),
+('Neymar', 'teste@gmail.com', '123456'),
+('Messi', 'teste@gmail.com', '123456'),
+('Cristiano', 'teste@gmail.com', '123456'),
+('Zidane', 'teste@gmail.com', '123456');
